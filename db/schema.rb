785db4b109191a44fb26a69e3a9773b00ab34095
ayaka.sano <ayaka@yahoo.co.jp>
2018-11-05 04:53:57 +0000

    bootstrap design

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,11 @@
 
 ActiveRecord::Schema.define(version: 20181101043024) do
 
-  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "title"
+    t.string   "status"
   end
 
 end
